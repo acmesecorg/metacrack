ea35326d1be5af469d4a6ba202aa42d139df4b5e
James Westgate <jameswestgate@users.noreply.github.com>
2021-09-28 17:48:40 +0100

    Add first pass stem plugin

---
+++ StemOptions.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+namespace Malfoy
+{
+    [Verb("stem", HelpText = "Creates an associated hash and wordlist by stemming user email information.")]
+    public class StemOptions
+    {        
+        [Value(0, Required = true, HelpText = "The path to the file(s) containing the input emails.")]
+        public string InputPath { get; set; }
+
+        [Option('n', "names")]
+        public string NamesPath { get; set; }
+
+        [Option("hash")]
+        public int Hash { get; set; }
+    }
+}
