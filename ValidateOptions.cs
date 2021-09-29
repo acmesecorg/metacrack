33b6822a8d5fd319c8f36c82530ab1910a576165
James Westgate <jameswestgate@users.noreply.github.com>
2021-09-29 12:13:01 +0100

    Add a validate plugin

---
+++ ValidateOptions.cs
@@ -0,0 +1,20 @@
+﻿using CommandLine;
+
+namespace Malfoy
+{
+    [Verb("validate", HelpText = "Creates a hash list with a mapped associated wordlist.")]
+    public class ValidateOptions
+    {
+        [Value(0, Required = true, HelpText = "The path to the file(s) containing the input values.")]
+        public string InputPath { get; set; }
+
+        [Option("hash", Required = true)]
+        public int Hash { get; set; }
+
+        [Option("iteration")]
+        public int Iteration { get; set; }
+
+        [Option("Column", Default = 1)]
+        public int Column { get; set; }
+    }
+}
---
+++ ValidatePlugin.cs
@@ -0,0 +1,88 @@
+﻿namespace Malfoy
+{
+    public class ValidatePlugin : PluginBase
+    {
+        private static string _outputValidPath = "";
+        private static string _outputInvalidPath = "";
+
+        public static void Process(ValidateOptions options)
+        {
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var fileEntries = Directory.GetFiles(currentDirectory, options.InputPath);
+
+            if (fileEntries.Length == 0)
+            {
+                WriteMessage($"Lookup file(s) {options.InputPath} not found.");
+                return;
+            }
+
+            WriteMessage($"Validating hash mode {options.Hash}.");
+
+            WriteMessage($"Started at {DateTime.Now.ToShortTimeString()}.");
+
+            var size = GetFileEntriesSize(fileEntries);
+            var progressTotal = 0L;
+            var lineCount = 0;
+
+            foreach (var filePath in fileEntries)
+            {
+                //Create a version based on the file size, so that the hash and dict are bound together
+                var fileInfo = new FileInfo(filePath);
+                var fileName = Path.GetFileNameWithoutExtension(filePath);
+                var filePathName = $"{currentDirectory}\\{fileName}";
+
+                _outputValidPath = $"{filePathName}.valid{fileInfo.Extension}";
+                _outputInvalidPath = $"{filePathName}.invalid{fileInfo.Extension}";
+
+                //Check that there are no output files
+                if (!CheckForFiles(new string[] { _outputValidPath, _outputInvalidPath }))
+                {
+                    WriteHighlight($"Skipping {filePathName}.");
+
+                    progressTotal += fileInfo.Length;
+                    continue;
+                }
+
+                var valid = new List<string>();
+                var invalid = new List<string>();
+
+                //Loop through and check each hash
+                using (var reader = new StreamReader(filePath))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var splits = line.Split(':');
+
+                        if (splits.Length > options.Column)
+                        {
+                            if (ValidateHash(splits[options.Column], options.Hash))
+                            {
+                                valid.Add(line);
+                            }
+                            else
+                            {
+                                invalid.Add(line);
+                            }
+                        }
+                        else
+                        {
+                            invalid.Add(line);
+                        }
+
+                        lineCount++;
+                        progressTotal += line.Length;
+
+                        //Update the percentage
+                        WriteProgress("Processing files", progressTotal, size);
+                    }
+                }
+
+                File.AppendAllLines(_outputValidPath, valid);
+                File.AppendAllLines(_outputInvalidPath, invalid);
+            }
+
+            WriteMessage($"Completed at {DateTime.Now.ToShortTimeString()}.");            
+        }
+    }
+}
