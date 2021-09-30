bc15cc8af6a4bd8303e9f16dc727b9af6419ab77
James Westgate <jameswestgate@users.noreply.github.com>
2021-09-30 11:23:49 +0100

    Add rank plugin

---
+++ RankOptions.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+
+namespace Malfoy
+{
+    [Verb("rank", HelpText = "Ranks a list of hash:plain text but the occurence of plain values.")]
+    public class RankOptions
+    {
+        [Value(0, Required = true, HelpText = "The path to the file(s) containing the hash:plains.")]
+        public string InputPath { get; set; }
+
+        [Option("Count", Default = 10)]
+        public int Count { get; set; }
+    }
+}
