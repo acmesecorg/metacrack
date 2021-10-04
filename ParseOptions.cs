4802ee3c8338204ae1c7ce30ed6cd46c638a5a1b
James Westgate <jameswestgate@users.noreply.github.com>
2021-10-04 16:32:42 +0100

    Upgrade parse options

--- ParseOptions.cs
+++ ParseOptions.cs
@@ -8,14 +8,17 @@ namespace Malfoy
         [Value(0, Required = true, HelpText = "The path to the file(s) containing the hash:plains.")]
         public string InputPath { get; set; }
 
-        [Option("type", Default = 0)]
-        public int ParseType { get; set; }
+        [Option("type", Default = "delimited")]
+        public string ParseType { get; set; }
 
         [Option("delimiter", Default = ",")]
         public string Delimiter { get; set; }
 
-        [Option]
+        [Option("columns")]
         public IEnumerable<string> Columns { get; set; }
+
+        [Option("names")]
+        public IEnumerable<string> Names { get; set; }
 
         [Option("deduplicate")]
         public bool Deduplicate { get; set; }
--- ParsePlugin.cs
+++ ParsePlugin.cs
@@ -13,6 +13,20 @@
                 return;
             }
 
+            var names = new List<string>();
+            
+            if (options.ParseType == "log" && options.Names.Count() == 2)
+            {
+                foreach (var name in options.Names) names.Add($"{name}:");
+            }
+            else
+            {
+                names.Add("Username:");
+                names.Add("Password:");
+            }
+            
+            if (options.ParseType == "log") WriteMessage($"Using names {String.Join(",", names)}.");
+
             var size = GetFileEntriesSize(fileEntries);
             var progressTotal = 0L;
 
@@ -58,12 +72,12 @@
                         lineCount++;
                         progressTotal += line.Length + 2;
 
-                        if (options.ParseType == 0)
+                        if (options.ParseType.ToLower() == "log")
                         {
                             //Password must follow line after email, or everythign is reset
-                            if (line.StartsWith("Username:"))
+                            if (line.StartsWith(names[0]))
                             {
-                                var token = line.Split("Username:", StringSplitOptions.TrimEntries)[1];
+                                var token = line.Split(names[0], StringSplitOptions.TrimEntries)[1];
                                 if (ValidateEmail(token, out var emailSteam))
                                 {
                                     email = emailSteam;
@@ -73,9 +87,9 @@
                                     notparsed.Add(line);
                                 }
                             }
-                            else if (line.StartsWith("Password:") && email != "")
+                            else if (line.StartsWith(names[1]) && email != "")
                             {
-                                var password = line.Split("Password:", StringSplitOptions.TrimEntries)[1];
+                                var password = line.Split(names[1], StringSplitOptions.TrimEntries)[1];
 
                                 output.Add($"{email}:{password}");
                                 email = "";
@@ -87,7 +101,7 @@
                             }
                         }
                         //Split by delimiter
-                        else if (options.ParseType == 1)
+                        else if (options.ParseType == "delimited")
                         {
                             var splits = line.Split(options.Delimiter, StringSplitOptions.TrimEntries);
                             var values = new List<string>();
