39f9e81e297bf4b2c9b96d981c0036fec7a43f7f
James Westgate <jameswestgate@users.noreply.github.com>
2021-10-14 22:46:12 +0100

    Add sort plugin

---
+++ SortOptions.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace Malfoy
+{
+    [Verb("sort", HelpText = "Ranks a list of hash:plain text but the occurence of plain values.")]
+    public class SortOptions
+    {
+        [Value(0, Required = true, HelpText = "The path to the file(s) containing the data to be sorted")]
+        public string InputPath { get; set; }
+
+        [Option("deduplicate", HelpText = "Deduplicates the file")]
+        public bool Deduplicate { get; set; }
+
+    }
+}
