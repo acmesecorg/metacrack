7a1237cd2b3d1e510e6b1fb3480ccd46eb8aff0d
James Westgate <jameswestgate@users.noreply.github.com>
2022-06-24 11:47:59 +0100

    Add support for hashcat modes

--- PluginBase.cs
+++ PluginBase.cs
@@ -198,39 +198,67 @@ namespace Metacrack
 
         public static HashInfo GetHashInfo(int mode)
         {
-            //8743b52063cd84097a65d1633f5c74f5
-            if (mode == 0) return new HashInfo(mode, 1, 32, true);
-            if (mode == 11) return new HashInfo(mode, 2, 32, true);
-            if (mode == 20) return new HashInfo(mode, 2, 32, true);
+            //0      8743b52063cd84097a65d1633f5c74f5  MD5
+            //70     2303b15bfa48c74a74758135a0df1201  md5(utf16le($pass))
+            if (mode == 0 || mode == 70) return new HashInfo(mode, 1, 32, true);
 
-            //b89eaac7e61417341b710b727768294d0e6a277b
-            if (mode == 100) return new HashInfo(mode, 1, 40, true);
+            //10     01dfae6e5d4d90d9892622325959afbe:7050461         md5($pass.$salt)
+            //20     f0fda58630310a6dd91a7d8f0a4ceda2:4225637426      md5($salt.$pass)
+            //30     b31d032cfdcf47a399990a71e43c5d2a:144816          md5(utf16le($pass).$salt)
+            //40     d63d0e21fdc05f618d55ef306c54af82:13288442151473  md5($salt.utf16le($pass))
+            //50     fc741db0a2968c39d9c2a5cc75b05370:1234            HMAC-MD5 (key = $pass)
+            //60     bfd280436f45fa38eaacac3b00518f29:1234            HMAC-MD5 (key = $salt)
+            if (mode == 10 || mode ==20 || mode == 30 || mode == 40 || mode == 50 || mode == 60) return new HashInfo(mode, 2, 32, true);
 
-            //2fc5a684737ce1bf7b3b239df432416e0dd07357:2014
-            if (mode == 110) return new HashInfo(mode, 2, 40, true);
-            if (mode == 120) return new HashInfo(mode, 2, 40, true);
+            //100    b89eaac7e61417341b710b727768294d0e6a277b  SHA1
+            //170  	 b9798556b741befdbddcbf640d1dd59d19b1e193  sha1(utf16le($pass))
+            //300    fcf7c1b8749cf99d88e5f34271d636178fb5d130  MySQL4.1/MySQL5
+            if (mode == 100 || mode == 170 || mode == 300) return new HashInfo(mode, 1, 40, true);
 
-            //$P$984478476IagS59wHZvyQMArzfx58u.
+            //110    2fc5a684737ce1bf7b3b239df432416e0dd07357:2014           sha1($pass.$salt)
+            //120    cac35ec206d868b7d7cb0b55f31d9425b075082b:5363620024     sha1($salt.$pass)
+            //130    c57f6ac1b71f45a07dbd91a59fa47c23abcd87c2:631225         sha1(utf16le($pass).$salt)
+            //140    5db61e4cd8776c7969cfd62456da639a4c87683a:8763434884872  sha1($salt.utf16le($pass))
+            //150    c898896f3f70f61bc3fb19bef222aa860e5ea717:1234           HMAC-SHA1 (key = $pass)
+            //160    d89c92b4400b15c39e462a8caa939ab40c3aeeea:1234           HMAC-SHA1 (key = $salt)
+            if (mode == 110 || mode == 120 || mode == 130 || mode == 140 || mode == 150 || mode == 160) return new HashInfo(mode, 2, 40, true);
+
+            //124    sha1$00003$2c5a9069be618cf209f7b21167d6e8cdd2dce76    
+            if (mode == 124) return new HashInfo(mode, 1, 51, false);
+
+            //400    $P$984478476IagS59wHZvyQMArzfx58u.  phpass, WordPress (MD5), Joomla(MD5)
             if (mode == 400) return new HashInfo(mode, 1, 34, false);
 
-            if (mode == 1400) return new HashInfo(mode, 1, 64, true);
-            if (mode == 1410) return new HashInfo(mode, 2, 64, true);
+            //1400   127e6fbfe24a750e72930c220a8e138275656b8e5d8f48a98c3c92df2caba935  SHA2-256
+            //1470   9e9283e633f4a7a42d3abc93701155be8afe5660da24c8758e7d3533e2f2dc82  sha256(utf16le($pass))
+            if (mode == 1400 || mode == 1470) return new HashInfo(mode, 1, 64, true);
 
+            //1410   c73d08de890479518ed60cf670d17faa26a4a71f995c1dcc978165399401a6c4:53743528  	sha256($pass.$salt)
+            //1420   eb368a2dfd38b405f014118c7d9747fcc97f4f0ee75c05963cd9da6ee65ef498:560407001617  sha256($salt.$pass)
+            //1430   4cc8eb60476c33edac52b5a7548c2c50ef0f9e31ce656c6f4b213f901bc87421:890128        sha256(utf16le($pass).$salt)
+            //1440   a4bd99e1e0aba51814e81388badb23ecc560312c4324b2018ea76393ea1caca9:12345678      sha256($salt.utf16le($pass))
+            //1450   abaf88d66bf2334a4a8b207cc61a96fb46c3e38e882e6f6f886742f688b8588c:1234          HMAC-SHA256 (key = $pass)
+            //1460   8efbef4cec28f228fa948daaf4893ac3638fbae81358ff9020be1d7a9a509fc6:1234          HMAC-SHA256 (key = $salt)
+            if (mode == 1410 || mode == 1420 || mode == 1430 || mode == 1440 || mode == 1450 || mode == 1460) return new HashInfo(mode, 2, 64, true);
+
+            //1800	 $6$52450745$k5ka2p8bFuSmoVT1tzOyyuaREkkKBcCNqoDKzYiJL9RaE8yMnPgh2XzzF0NDrUhgrcLwg78xs1w5pJiypEdFX/  sha512crypt $6$, SHA512 (Unix)
             if (mode == 1800) return new HashInfo(mode, 1, 106, false);
 
-            //b2771af9d6e8395c72254bbc379dd092:NqPyawIn
+            //2811   b2771af9d6e8395c72254bbc379dd092:NqPyawIn  
             if (mode == 2811) return new HashInfo(mode, 2, 32, true);
 
-            //$2a$05$LhayLxezLhK1LhWvKxCyLOj0j1u.Kj0jZ0pEmm134uzrQlFvQJLF6
+            //3200   $2a$05$LhayLxezLhK1LhWvKxCyLOj0j1u.Kj0jZ0pEmm134uzrQlFvQJLF6  bcrypt $2*$, Blowfish (Unix)
             if (mode == 3200) return new HashInfo(mode, 1, 60, false);
 
             if (mode == 2611) return new HashInfo(mode, 2, 32, true);
 
-            //pbkdf2_sha256$20000$H0dPx8NeajVu$GiC4k5kqbbR9qWBlsRgDywNqC2vd9kqfk7zdorEnNas=
+            //10000  pbkdf2_sha256$20000$H0dPx8NeajVu$GiC4k5kqbbR9qWBlsRgDywNqC2vd9kqfk7zdorEnNas=  Django (PBKDF2-SHA256)
             if (mode == 10000) return new HashInfo(mode, 1, 77, 79, false);
 
+            //25600  $2a$05$/VT2Xs2dMd8GJKfrXhjYP.DkTjOVrY12yDN7/6I8ZV0q/1lEohLru  bcrypt(md5($pass))/bcryptmd5
             if (mode == 25600) return new HashInfo(mode, 1, 60, false, "$2");
 
+            //27200  3999d08db95797891ec77f07223ca81bf43e1be2:5dcc47b04c49d3c8e1b9e4ec367fddeed21b7b85  Ruby on Rails Restful Auth (one round, no sitekey)
             if (mode == 27200) return new HashInfo(mode, 2, 40, true);
 
             //Return a default setting with zero length
