bffdc8d1f9620708bdd05a2711a756bdd569b16a
James Westgate <jameswestgate@users.noreply.github.com>
2022-09-05 20:16:20 +0100

    Add to tutorial samples for sql plugin

---
+++ Tutorial/sample1.sql
@@ -0,0 +1,6 @@
+INSERT INTO MyUsers
+    (Id, Email, Hash, Username, Salt)
+VALUES 
+    (1, 'joe@blogs.com', 'e76e194b7b75987f1bc8b54dc9349277', 'Joe', 'c808zlYhCO5vBQRagXEhDZGsVlvJv0'),
+    (2, 'joe@soap.com', '7fa65c550919b2a45b82131eda56e9de', 'Soapster', 'BBT1ezmsegtaEkJStRbd8uRPkcPfiP'),
+    (3, 'joe@blow.org', 'f3ae38b17addc9efc539e719554d0b87', 'movieguy', 'f3ae38b17addc9efc539e719554d0b87')
